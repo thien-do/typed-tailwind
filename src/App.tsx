97600380c0b992f62c34360dded0e092ae18e4ed
dvkndn <dvkndn@gmail.com>
2019-09-30 16:55:46 +0700

    refactor: import tailwind from css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 
 const App: React.FC = () => (
   <div className="App">
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import './tailwind/dist.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
