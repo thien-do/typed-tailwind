03a96dcff21f5efa43ab9f891ff9f20be051ec5f
dvkndn <dvkndn@gmail.com>
2019-10-03 23:15:32 +0700

    refactor: styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./index.css";
+import "./style/style.css";
 import { App } from "./app/app";
 // import * as serviceWorker from "./serviceWorker";
 
