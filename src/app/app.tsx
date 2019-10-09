01bb61b753b3b16a4627d790040df707e3a674d1
dvkndn <dvkndn@gmail.com>
2019-10-09 23:01:06 +0700

    [feat] add github icon

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,7 +6,7 @@ import { Title } from "./title";
 
 export const App: React.FC = () => {
   return (
-    <div className={Tw().bg3().hScreen().lgFlex().flexCol().py32().$()}>
+    <div className={Tw().bg2().hScreen().lgFlex().flexCol().py32().$()}>
       <div className={Tw().order1().flexNone().py32().$()}><Title /></div>
       <div className={Tw().order0().flex1().overflowHidden().$()}><Convert /></div>
     </div>
